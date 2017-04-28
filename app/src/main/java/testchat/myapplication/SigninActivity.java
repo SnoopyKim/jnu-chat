ed9c4dacea8edd4c0024df9692509233a281df27
cherrue <th885172@gmail.com>
2017-04-28 13:32:14 +0900

    complete find info activity layout

---
+++ app/src/main/java/testchat/myapplication/SigninActivity.java
@@ -0,0 +1,19 @@
+package testchat.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by th on 2017-04-28.
+ */
+
+public class SigninActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_findinfo);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+    }
+}
