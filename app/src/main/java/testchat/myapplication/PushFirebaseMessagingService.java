7f63253163d74989f3ea150dcd7b1be32dc13949
cherrue <th885172@gmail.com>
2017-06-13 11:17:19 +0900

    commit for merge with jh_test

--- app/src/main/java/testchat/myapplication/PushFirebaseMessagingService.java
+++ app/src/main/java/testchat/myapplication/PushFirebaseMessagingService.java
@@ -9,6 +9,7 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -25,23 +26,30 @@ public class PushFirebaseMessagingService extends com.google.firebase.messaging.
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Map<String,String> data = remoteMessage.getData();
-        sendPushNotification(remoteMessage.getData().get("message"));
-        String title = data.get("title");
-        String msg = data.get("message");
+        RemoteMessage.Notification data = remoteMessage.getNotification();
+        String title = data.getTitle();
+        String msg = data.getBody();
+        sendPushNotification(title,msg);
+        Log.d(TAG,"Title:"+title);
+        Log.d(TAG,"Message:"+msg);
     }
 
-    private void sendPushNotification(String message){
+    private void sendPushNotification(String title,String message){
         System.out.println("received message : " + message);
+        //up MainActivity
+        //flag activity clear top : set piriorty 1st
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //define intent work
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
+        //ringing class
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        //alarm service setting
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_person_black_24dp).setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.ic_jnu_chat) )
-                .setContentTitle("Push Title ")
+                .setSmallIcon(R.drawable.ic_jnu_chat).setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.ic_jnu_chat) )
+                .setContentTitle(title)
                 .setContentText(message)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri).setLights(000000255,500,2000)
